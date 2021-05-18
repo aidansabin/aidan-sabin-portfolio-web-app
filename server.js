c6be2d8b2dc206d43148ef3ea4550044b4f70a0b
Aidan Sabin <aidansabin@icloud.com>
2021-05-18 01:18:14 +0100

    fix: put mongoose connect above cors

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ var dns = require('dns');
 var app = express();
 var port = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
